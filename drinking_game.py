ec851a349f832efab8e0809209dc4bf98be6e220
Seph Cordovano <jcordovano@tracerecon.com>
2018-04-24 21:44:09 -0400

    Updated for fault tolerance

--- drinking_game.py
+++ drinking_game.py
@@ -13,35 +13,67 @@ Guess the number correctly and it's a 3 or 7 reverse the order
 of dinks consumed and passed out.
 """)
 
-# generate a random num between 1 and 10
-secret_num = random.randint(1, 10)
-reverse_flag = (True if secret_num == 3 or secret_num == 7 else False )
+def game():
+    # generate a random num between 1 and 10
+    secret_num = random.randint(1, 10)
 
-max_guesses = 3
-guess_count = 0
-while guess_count < max_guesses:
-    remaining_guesses = max_guesses - guess_count
+    # reversed if number is special number ( 3 or 7 )
+    reverse_flag = (True if secret_num == 3 or secret_num == 7 else False )
 
-    # pluralize output message
-    pluralization = ("" if max_guesses - guess_count == 1 else "es")
+    max_guesses = 3
+    guess_count = 0
+    while guess_count < max_guesses:
+        remaining_guesses = max_guesses - guess_count
 
-    # get a number guess from player
-    guess = int(input("{} remaining guess{}. Guess a number between 1 and 10: ".format(remaining_guesses, pluralization)))
+        # pluralize output message
+        pluralization = ("" if max_guesses - guess_count == 1 else "es")
 
-    # compare guess to secret number
-    if guess == secret_num:
-        if guess_count == 0 and reverse_flag:
-            sys.exit("First try! But the number was {} so you have to take {} drink(s)".format(secret_num, max_guesses * 2))
+        try:
+            # get a number guess from player
+            guess = input("{} remaining guess{}. Guess a number between 1 and 10: ".format(remaining_guesses, pluralization))
+            guess = int(guess)
+            if guess < 1 or guess > 10: raise Exception
+        except ValueError:
+            print("{} isn't a number!".format(guess))
+        except Exception:
+            print('Number out of range')
         else:
-            sys.exit("First try! Everyone takes {}".format(max_guesses * 2))
+            # correct guess
+            if guess == secret_num:
+                # on first try
+                if guess_count == 0:
+                    if reverse_flag:
+                        print("First try! But the number was {} so you have to take {} drink(s)".format(secret_num, max_guesses * 2))
+                        break
+                    else:
+                        print("First try! Everyone takes {}".format(max_guesses * 2))
+                        break
+                # after first try
+                else:
+                    if reverse_flag:
+                        print("Reverse, reverse! Take {} and pass out {} drink(s)".format(remaining_guesses, guess_count))
+                        break
+                    else:
+                        print("You got it! Take {} drink(s) and pass out {}".format(guess_count, remaining_guesses))
+                        break
 
-        if reverse_flag:
-            sys.exit("Reverse, reverse! Take {} and pass out {} drink(s)".format(remaining_guesses, guess_count))
-    elif guess > secret_num:
-        print("Miss, guess lower!")
-    elif guess < secret_num:
-        print("Miss, guess higher!")
+            # incorrect guess
+            else:
+                guess_count += 1
+                if guess_count == max_guesses:
+                    print("You lose. The number was {}. Drink {} sips".format(secret_num, max_guesses))
+                    break
+                else:
+                    if guess > secret_num:
+                        print("Miss, guess lower!")
+                    elif guess < secret_num:
+                        print("Miss, guess higher!")
 
-    guess_count += 1
 
-print("You lose. The number was {}. Drink {} sips".format(secret_num, max_guesses))
+    play_again = input("Do you want to play again? Y/n ")
+    if play_again.lower() != 'n':
+        game()
+    else:
+        print("Bye!")
+
+game()
